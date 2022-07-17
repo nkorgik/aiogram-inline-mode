d4c8e66a459def6773b4cbe2483464b4828ff741
nkorgik <nkorgik639@gmail.com>
2022-07-17 11:30:10 +0300

    added core_simple_counter CallbackData Factory pattern

--- callback_1/core_simple_counter.py
+++ callback_1/core_simple_counter.py
@@ -3,6 +3,7 @@ import pprint
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.callback_data import CallbackData
 
 from config import TOKEN_API
 
@@ -13,12 +14,15 @@ ikb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Random', callback_data='random_value')]
 ])
 
+
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message) -> None:
+# implementation with using simple callback_data: str #
+
+@dp.message_handler(commands=['counter'])
+async def cmd_counter(message: types.Message) -> None:
     await bot.send_message(chat_id=message.chat.id,
                            text='Number is 0',
                            reply_markup=ikb)
@@ -52,6 +56,45 @@ async def ikb_increase_cb_handler(callback: types.CallbackQuery) -> None:
 
     await callback.message.edit_text(f'The number is {random_number} at the moment',
                                      reply_markup=ikb)
+
+
+# implementation with using Callback Data Factory pattern | callback_data: dict #
+
+cb = CallbackData('number', 'action')  # here we've created new "callback" data factory pattern instance
+user_data = dict()  # in this dictionary we're gonna store our random_number here that's got to do with our user model
+
+def get_keyboard() -> InlineKeyboardMarkup:  # unfinished
+    ikb_2 = InlineKeyboardMarkup(inline_keyboard=[  # here we have created our inlinekeyboardmarkup obviously
+        [InlineKeyboardButton('Increase', callback_data=cb.new('increase')),
+         InlineKeyboardButton('Decrease', callback_data='decrease')],
+        [InlineKeyboardButton('Random', callback_data=cb.new('random'))]
+    ])
+    return ikb_2
+
+
+@dp.message_handler(commands=['callback'])  # this handler is going to be processing "/callback" command and "reply" ikb_2 we have created above
+async def cmd_callback(message: types.Message) -> None:
+    user_data[message.from_user.id] = 0
+    await bot.send_message(chat_id=message.chat.id,
+                           reply_markup=get_keyboard(),
+                           text='The current number is - 0')
+
+
+@dp.callback_query_handler(cb.filter())  # there's no need to use sophisticated lambda filters anymore, we just implement new built-in filter in this place that significantly simplify our work
+async def ikb2_increase_cb_handler(callback: types.CallbackQuery, callback_data: dict) -> None:
+    global user_data
+    if callback_data['action'] == 'increase':
+        user_data[callback.from_user.id] += 1
+        await callback.message.edit_text(f'The current number is - {user_data[callback.from_user.id]}',
+                                         reply_markup=get_keyboard())
+    elif callback_data['action'] == 'decrease':
+        user_data[callback.from_user.id] -= 1
+        await callback.message.edit_text(f'The current number is - {user_data[callback.from_user.id]}',
+                                         reply_markup=get_keyboard())
+
+    user_data[callback.from_user.id] = random.randint(1, 50)
+    await callback.message.edit_text(f'The current number is - {user_data[callback.from_user.id]}',
+                                     reply_markup=get_keyboard())
 
 
 if __name__ == '__main__':
